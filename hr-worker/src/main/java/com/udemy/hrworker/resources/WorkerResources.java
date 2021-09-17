3e0194e55a830b83ebf148ca4181a239db54ce1b
Kauê Oliveira <kso13@hotmail.com>
2021-09-17 02:23:34 -0300

    Implementação hr-worker

---
+++ hr-worker/src/main/java/com/udemy/hrworker/resources/WorkerResources.java
@@ -0,0 +1,49 @@
+package com.udemy.hrworker.resources;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.hrworker.entities.Worker;
+import com.udemy.hrworker.entities.WorkerRepository;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResources {
+
+	@Autowired
+	private WorkerRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Worker>> findAll(){
+		
+		List<Worker> list = repository.findAll();
+		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		
+		Worker obj = repository.findById(id).get();
+
+		
+		return ResponseEntity.ok(obj);
+
+		
+		
+		//		return Optional.ofNullable(obj).map(ResponseEntity.ok(obj)).orElse(ResponseEntity.noContent());
+		//		
+//		ResponseEntity<Worker> okOrNot = null; 
+//		
+//		Optional<Worker> worker = repository.findById(id);
+//		worker.ofNullable((ResponseEntity<Optional<Worker>>) okOrNot.notFound()).orElse((ResponseEntity<Optional<Worker>>)okOrNot.ok(worker));
+//	
+//		return Optional.ofNullable(worker).map(ResponseEntity.ok(worker)).orElse(ResponseEntity.notFound());
+	}
+}
