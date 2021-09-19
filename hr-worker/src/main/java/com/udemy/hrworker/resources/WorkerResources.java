378d39aa29f649589acb28c3e65188d42c184cb0
Kauê Oliveira <kso13@hotmail.com>
2021-09-19 18:52:06 -0300

    Logger no hr-worker

--- hr-worker/src/main/java/com/udemy/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/udemy/hrworker/resources/WorkerResources.java
@@ -2,7 +2,10 @@ package com.udemy.hrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,11 @@ import com.udemy.hrworker.entities.WorkerRepository;
 @RequestMapping(value = "/workers")
 public class WorkerResources {
 
+	private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
+	
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	private WorkerRepository repository;
 	
@@ -29,9 +37,10 @@ public class WorkerResources {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		
-		Worker obj = repository.findById(id).get();
-
+		logger.info("PORT = "+ env.getProperty("local.server.port"));
 		
+		
+		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 
 	}
