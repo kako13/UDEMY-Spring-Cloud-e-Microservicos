7bb53d9a181617949fecc45a31b7e55a3733dbf0
Kauê Oliveira <kso13@hotmail.com>
2021-09-20 04:57:08 -0300

    Zuul Timeout

--- hr-worker/src/main/java/com/udemy/hrworker/resources/WorkerResources.java
+++ hr-worker/src/main/java/com/udemy/hrworker/resources/WorkerResources.java
@@ -20,34 +20,33 @@ import com.udemy.hrworker.entities.WorkerRepository;
 public class WorkerResources {
 
 	private static Logger logger = LoggerFactory.getLogger(WorkerResources.class);
-	
+
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private WorkerRepository repository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Worker>> findAll(){
-		
+	public ResponseEntity<List<Worker>> findAll() {
+
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
 	}
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Worker> findById(@PathVariable Long id){
+	public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+		// Teste forcando a espera de 3 segundos
 		
-		//Teste forcando a espera de 3 segundos
-		/*
-		 * try { 
-		 * Thread.sleep(3000L); 
-		 * } catch (InterruptedException e) {
-		 * e.printStackTrace(); 
-		 * }
-		 */
-		logger.info("PORT = "+ env.getProperty("local.server.port"));
-		
-		
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		 
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 
