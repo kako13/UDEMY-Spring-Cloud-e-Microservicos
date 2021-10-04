837c8aee33a5d820ad980aad0cf3a1c72f2f5809
Kauê Oliveira <kso13@hotmail.com>
2021-10-03 23:07:57 -0300

    Login, JWT token, dependencias, security config

--- hr-oauth/src/main/java/com/udemy/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/udemy/hroauth/entities/User.java
@@ -1,10 +1,16 @@
 package com.udemy.hroauth.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class User implements Serializable{
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails, Serializable{
 	private static final long serialVersionUID = 2654411443053476487L;
 	
 	private Long id;
@@ -90,5 +96,36 @@ public class User implements Serializable{
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", roles=" + roles
 				+ "]";
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName())).collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 }
--- hr-oauth/src/main/java/com/udemy/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/udemy/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.udemy.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.udemy.hroauth.clients.UserFeignClient;
 import com.udemy.hroauth.entities.User;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 
 	Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -25,6 +28,19 @@ public class UserService {
 			throw new IllegalAccessException("Email not found");
 		}
 		logger.info("Email found: " + email);
+		return user;
+	}
+
+	//metodo duplicado apenas para estudos, o correto seria apenas este
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findById(username).getBody();
+		
+		if (user == null) {
+			logger.error("Email not foud: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: " + username);
 		return user;
 	}
 	
